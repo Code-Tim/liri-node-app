7c02dc436642770358c11b99d0d7a2c8f532168d
Code-Tim <62041831+Code-Tim@users.noreply.github.com>
2020-06-09 23:02:49 -0400

    added the default setting fro spotify search

--- liri.js
+++ liri.js
@@ -74,6 +74,10 @@ function concertThis(artist = "Celine Dion") {
 // This will show the following information about the song in your terminal/bash window
 // node liri.js spotify-this-song '<song name here>'
 function spotThis() {
+    // If no song is provided then your program will default to "The Sign" by Ace of Base.
+    if (!secondInput) {
+        secondInput = 'The Sign, Ace of Base';
+    }
 
     spotify
         .search({ type: 'track', query: secondInput })
@@ -92,21 +96,6 @@ function spotThis() {
             console.log(err);
         });
 }
-
-// If no song is provided then your program will default to "The Sign" by Ace of Base.
-
-// You will utilize the node-spotify-api package in order to retrieve song information from the Spotify API.
-
-// The Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a client id and client secret:
-
-// Step One: Visit https://developer.spotify.com/my-applications/#!/
-
-// Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
-
-// Step Three: Once logged in, navigate to https://developer.spotify.com/my-applications/#!/applications/create to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
-
-// Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the node-spotify-api package.
-
 // node liri.js movie-this '<movie name here>'
 
 // This will output the following information to your terminal/bash window:
